d99b41765064267ea160d7858543696ea0eb0b35
Brendan Witt <Brendan@Brendans-MacBook-Pro.local>
2019-11-21 15:05:39 -0500

    Added more of a skeleton and notes on what to do

--- Assets/Scripts/CelestialBody.cs
+++ Assets/Scripts/CelestialBody.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CelestialBody : MonoBehaviour
 {
+    // Member variables
+    public int InternalID;
+    public double[] Position = new double[3];
+    public double[] Velocity = new double[3];
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Update x, y, z position values based on speed here? Maybe assign
+        //   velocity vectors elsewhere that the bodies will travel on
     }
+
+
 }
---
+++ Assets/Scripts/CelestialGroup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CelestialGroup : MonoBehaviour
+{
+    // public GameObject[] CelestialBodyArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Call a function to take in and create objects from JSON data in
+        //   CleanedData.json -- there should be C# libraries to do this w/ JSON
+
+        // Instantiate all of the necessary CelestialBody objects into the scene
+        //   and store them in public array above
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public var ConvertFromJSON(string filename)
+    {
+        // Write something here to maybe convert the CleanedData.json file and
+        //   export like an array/vector of satellite objects (class CelestialBody???)
+    }
+}
