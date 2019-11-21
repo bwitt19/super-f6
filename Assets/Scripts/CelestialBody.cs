93341af4b9ba17d18c46bf2d6b99d3b8ff33425d
Brendan Witt <bwitt1@umbc.edu>
2019-11-21 18:34:50 -0500

    Additional notes made on what functionality to build out

--- Assets/Scripts/CelestialBody.cs
+++ Assets/Scripts/CelestialBody.cs
@@ -8,11 +8,14 @@ public class CelestialBody : MonoBehaviour
     public int InternalID;
     public double[] Position = new double[3];
     public double[] Velocity = new double[3];
+    private bool Focus = False;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        // Create a trail based on the trajectory the object will take (can do
+        //   after everything else is done -- eventual functionality)
     }
 
     // Update is called once per frame
@@ -22,5 +25,11 @@ public class CelestialBody : MonoBehaviour
         //   velocity vectors elsewhere that the bodies will travel on
     }
 
+    /*
+    Eventual functionality to put in an onClick() type function: setting focus
+    on a singular CelestialBody and allowing it to shine through the rest.
+    */
+
+    void setFocus(bool newFocus) { Focus = newFocus; }
 
 }
